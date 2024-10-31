f55f71feb3660fa3a7efbda4fb15689e2a5ebf41
Leg0Batman <159012019+Leg0Batman@users.noreply.github.com>
2024-10-31 23:07:55 +0100

    Refactor server.go to listen on port 6379 and respond with "+PONG\r\n" for incoming connections

--- app/server.go
+++ app/server.go
@@ -1,52 +1,36 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"os"
 )
 
 func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
+	//Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	defer l.Close()
-
+	conn, err := l.Accept()
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			continue
 		}
-
-		go handleConnection(conn)
+		go handleRequest(conn)
 	}
 }
-
-func handleConnection(conn net.Conn) {
+func handleRequest(conn net.Conn) {
 	defer conn.Close()
-	reader := bufio.NewReader(conn)
-
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading from connection: ", err.Error())
-			return
-		}
-
-		// Check if the command is PING
-		if line == "PING\r\n" {
-			// Respond with "+PONG\r\n"
-			_, err = conn.Write([]byte("+PONG\r\n"))
-			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				return
-			}
-		}
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
 	}
+	conn.Write([]byte("+PONG\r\n"))
 }
