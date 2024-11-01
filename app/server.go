20790189f711fb3892c41b2139d55c4090e16b5e
Leg0Batman <159012019+Leg0Batman@users.noreply.github.com>
2024-11-01 21:08:15 +0100

    Refactor server.go to listen on port 6379 and respond with "+PONG\r\n" for incoming connections

--- app/server.go
+++ app/server.go
@@ -9,13 +9,12 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-	//Uncomment this block to pass the first stage
+	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
 	for {
 		conn, err := l.Accept()
 		if err != nil {
@@ -25,12 +24,16 @@ func main() {
 		go handleRequest(conn)
 	}
 }
+
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
 	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+	for {
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			return
+		}
+		conn.Write([]byte("+PONG\r\n"))
 	}
-	conn.Write([]byte("+PONG\r\n"))
 }
