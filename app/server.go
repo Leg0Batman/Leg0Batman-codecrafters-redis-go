34935709359ebf9e486afddf2fcf29b1b9db8bcc
Leg0Batman <159012019+Leg0Batman@users.noreply.github.com>
2024-11-01 21:12:26 +0100

    Refactor server.go to handle incoming connections and respond with "+PONG\r\n" for PING command

--- app/server.go
+++ app/server.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -27,13 +27,75 @@ func main() {
 
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
-	buf := make([]byte, 1024)
+	reader := bufio.NewReader(conn)
 	for {
-		_, err := conn.Read(buf)
+		request, err := parseRequest(reader)
 		if err != nil {
 			fmt.Println("Error reading:", err.Error())
 			return
 		}
-		conn.Write([]byte("+PONG\r\n"))
+		if len(request) > 0 {
+			command := strings.ToUpper(request[0])
+			switch command {
+			case "PING":
+				conn.Write([]byte("+PONG\r\n"))
+			case "ECHO":
+				if len(request) > 1 {
+					conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(request[1]), request[1])))
+				} else {
+					conn.Write([]byte("-ERR wrong number of arguments for 'echo' command\r\n"))
+				}
+			default:
+				conn.Write([]byte(fmt.Sprintf("-ERR unknown command '%s'\r\n", command)))
+			}
+		}
 	}
+}
+
+func parseRequest(reader *bufio.Reader) ([]string, error) {
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	line = strings.TrimSpace(line)
+	if len(line) == 0 || line[0] != '*' {
+		return nil, fmt.Errorf("invalid request")
+	}
+	numArgs, err := parseInteger(line[1:])
+	if err != nil {
+		return nil, err
+	}
+	request := make([]string, numArgs)
+	for i := 0; i < numArgs; i++ {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		line = strings.TrimSpace(line)
+		if len(line) == 0 || line[0] != '$' {
+			return nil, fmt.Errorf("invalid request")
+		}
+		argLen, err := parseInteger(line[1:])
+		if err != nil {
+			return nil, err
+		}
+		arg := make([]byte, argLen)
+		_, err = reader.Read(arg)
+		if err != nil {
+			return nil, err
+		}
+		request[i] = string(arg)
+		// Read the trailing \r\n
+		_, err = reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+	}
+	return request, nil
+}
+
+func parseInteger(s string) (int, error) {
+	var n int
+	_, err := fmt.Sscanf(s, "%d", &n)
+	return n, err
 }
